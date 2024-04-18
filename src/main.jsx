bbd12663e9075a5d0b4c820e027464f021fd3455
Nepenthes-cigue <emmarobert8322@gmail.com>
2024-04-18 14:11:15 +0200

    React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import App from './App.jsx';
+
+import{
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+
+import './index.css';
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
+
+//Etape :
+//https://reactrouter.com/en/main/start/tutorial#the-root-route
